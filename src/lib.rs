c16f777494283633e36f29c7b894738f0b8f77c4
Teddy Heinen <teddy@heinen.dev>
2020-04-22 20:32:33 -0500

    clean up a bit and add Part struct instead of just string

--- src/lib.rs
+++ src/lib.rs
@@ -12,59 +12,77 @@ pub struct Chip {
     name: String,
     inputs: Vec<String>,
     outputs: Vec<String>,
-    parts: Vec<String>,
+    parts: Vec<Part>,
 }
 
-fn is_space_or_comma(chr: char) -> bool {
-    match chr {
-        ' ' | ',' => true,
-        _ => false
-    }
+#[derive(Debug)]
+pub struct Part {
+    name: String,
+    internal: Vec<String>,
+    external: Vec<String>,
 }
 
-fn part(text: &str) -> IResult<&str, &str> {
+// TODO make this less awful
+fn part(text: &str) -> IResult<&str, Part> {
     let (text, _) = take_until("\t")(text)?;
     let (text, _) = tag("\t")(text)?;
-    let (text, part) = take_until(";")(text)?;
-    let (text, _) = tag(";\n")(text)?;
+    let (text, name) = take_until("(")(text)?;
+    let (text, _) = tag("(")(text)?;
+    let (text, pins) = take_until(")")(text)?;
+    let pins2 = pins
+        .replace(" ", "")
+        .split(",")
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>();
+    let mut part = Part {
+        name: name.to_string(),
+        internal: vec![],
+        external: vec![],
+    };
+    for i in pins2 {
+        let a = i.split("=").collect::<Vec<&str>>();
+        part.internal.push(a[0].to_string());
+        part.external.push(a[1].to_string());
+    }
     return Ok((text, part))
 }
 
-fn inputs(text: &str) -> IResult<&str, &str> {
-    let (text, _) = take_while(is_space_or_comma)(text)?;
-    let (text, input) = take_till(is_space_or_comma)(text)?;
-    let (text, _) = take_while(is_space_or_comma)(text)?;
-    return Ok((text, input))
-}
-
-pub fn parse(text: &str) -> IResult<&str, Chip> {
-    let (text, _) = tag("CHIP ")(text)?;
-    let (text, chip_name) = take_until(" ")(text)?;
-    let (text, _) = take_until("IN ")(text)?;
+fn inputs(text: &str) -> IResult<&str, Vec<String>> {
     let (text, _) = tag("IN ")(text)?;
     let (text, inputs) = take_until(";")(text)?;
-    // let (Text, inputs) = many0(inputs)(text)?;
-    let (text, _) = tag(";")(text)?;
-    let (text, _) = take_until("OUT ")(text)?;
+    return Ok((text, inputs.replace(" ", "").split(",").map(|x| x.to_string()).collect::<Vec<String>>()))
+}
+
+fn outputs(text: &str) -> IResult<&str, Vec<String>> {
     let (text, _) = tag("OUT ")(text)?;
-    let (text, outputs) = take_until(";")(text)?;
-    let (text, _) = take_until("PARTS:\n")(text)?;
+    let (text, inputs) = take_until(";")(text)?;
+    return Ok((text, inputs.replace(" ", "").split(",").map(|x| x.to_string()).collect::<Vec<String>>()))
+}
+
+fn parts(text: &str) -> IResult<&str, Vec<Part>> {
     let (text, _) = tag("PARTS:\n")(text)?;
-
     let (text, parts) = many0(part)(text)?;
+    return Ok((text, parts))
+}
 
-    let inputs = inputs.replace(" ", "").split(",").map(|x| x.to_string()).collect::<Vec<String>>();
-    let outputs = outputs.replace(" ", "").split(",").map(|x| x.to_string()).collect::<Vec<String>>();
-    let parts = parts.iter().map(|x| x.to_string()).collect::<Vec<String>>();
-    // println!("CHIPNAME: {}", chip_name);
-    // println!("INPUTS: {:?}", inputs);
-    // println!("OUTPUTS: {:?}", outputs);
-    // println!("PARTS: {:?}", parts);
+pub fn parse_hdl(text: &str) -> IResult<&str, Chip> {
+    let (text, _) = tag("CHIP ")(text)?;
+    let (text, chip_name) = take_until(" ")(text)?;
+
+    let (text, _) = take_until("IN ")(text)?;
+    let (text, inputs) = inputs(text)?;
+
+    let (text, _) = take_until("OUT ")(text)?;
+    let (text, outputs) = outputs(text)?;
+
+    let (text, _) = take_until("PARTS:\n")(text)?;
+    let (text, parts) = parts(text)?;
+
     return Ok((text, Chip {
         name: chip_name.to_string(),
-        inputs: inputs,
-        outputs: outputs,
-        parts: parts,
+        inputs,
+        outputs,
+        parts,
     }));
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{fs, env};
-use hack_hdl::{Chip, parse};
+use hack_hdl::{Chip, parse_hdl};
 
 fn main() {
     let hdl = match fs::read_to_string(env::args().nth(1).unwrap()) {
@@ -7,7 +7,7 @@ fn main() {
         Err(e) => panic!("Could not read file {:?}: {:?}", env::args().nth(1).unwrap(), e),
     };
 
-    println!("{:?}", match parse(&hdl) {
+    println!("{:#?}", match parse_hdl(&hdl) {
         Ok(x) => x.1,
         Err(e) => panic!("{}",e),
     });
