03598f59efad2497565e451bd7022256b941eb19
Teddy Heinen <teddy@heinen.dev>
2020-05-21 15:31:28 -0500

    update use statement in parser.rs also oops

--- benches/parser.rs
+++ benches/parser.rs
@@ -1,5 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use hack_hdl_parser::parse_hdl;
+use nand2tetris_hdl_parser::parse_hdl;
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("parse_hdl example.hdl", |b| b.iter(|| parse_hdl(black_box(include_str!("../test_cases/example.hdl")))));
